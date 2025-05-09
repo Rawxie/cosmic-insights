3446be2d2b6b09dd42e8b2701f545c7b93de740a
Rawxie <harshilparikh-cse-student@itmbu.ac.in>
2025-05-09 11:09:35 +0530

    Update Vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    includeFiles: ['./src/pages/api/**/*'],
+    maxDuration: 5
+  }),
   integrations: [
     tailwind(),
     react()
